e2be82630ed7ee36501afb6175ab3be3d13173bd
mdorkhah <mda84@sfu.ca>
2022-03-15 07:44:20 -0700

    CI Test

---
+++ ci/v2/ci_test.py
@@ -0,0 +1,216 @@
+"""
+Integration test of the CMPT 756 sample applicaton.
+
+Result of test in program return code:
+0: Test succeeded
+1: Test failed
+"""
+
+# Standard library modules
+import argparse
+import os
+import sys
+
+# Installed packages
+import pytest
+
+# Local modules
+import create_tables
+import music
+import user
+import playlist
+
+# The services check only that we pass an authorization,
+# not whether it's valid
+DUMMY_AUTH = 'Bearer A'
+
+@pytest.fixture
+def song(request):
+    # Recorded 1956
+    return ('Elvis Presley', 'Hound Dog')
+
+def parse_args():
+    """Parse the command-line arguments.
+
+    Returns
+    -------
+    namespace
+        A namespace of all the arguments, augmented with names
+        'user_url', 'music_url' and 'playlist_url'.
+    """
+    argp = argparse.ArgumentParser(
+        'ci_test',
+        description='Integration test of CMPT 756 sample application'
+        )
+    argp.add_argument(
+        'user_address',
+        help="DNS name or IP address of user service."
+        )
+    argp.add_argument(
+        'user_port',
+        type=int,
+        help="Port number of user service."
+        )
+    argp.add_argument(
+        'music_address',
+        help="DNS name or IP address of music service."
+        )
+    argp.add_argument(
+        'music_port',
+        type=int,
+        help="Port number of music service."
+        )
+    argp.add_argument(
+        'playlist_address',
+        help="DNS name or IP address of playlist service."
+        )
+    argp.add_argument(
+        'playlist_port',
+        type=int,
+        help="Port number of playlist service."
+        )
+    argp.add_argument(
+        'table_suffix',
+        help="Suffix to add to table names (not including leading "
+             "'-').  If suffix is 'scp756-2022', the music table "
+             "will be 'Music-scp756-2022'."
+    )
+    args = argp.parse_args()
+    args.user_url = "http://{}:{}/api/v1/user/".format(
+        args.user_address, args.user_port)
+    args.music_url = "http://{}:{}/api/v1/music/".format(
+        args.music_address, args.music_port)
+    args.playlist_url = "http://{}:{}/api/v1/playlist/".format(
+        args.playlist_address, args.playlist_port)
+    return args
+
+
+def get_env_vars(args):
+    """Augment the arguments with environment variable values.
+
+    Parameters
+    ----------
+    args: namespace
+        The command-line argument values.
+
+    Environment variables
+    ---------------------
+    AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,
+        SVC_LOADER_TOKEN, DYNAMODB_URL: string
+        Environment variables specifying these AWS access parameters.
+
+    Modifies
+    -------
+    args
+        The args namespace augmented with the following names:
+        dynamodb_region, access_key_id, secret_access_key, loader_token,
+        dynamodb_url
+
+        These names contain the string values passed in the corresponding
+        environment variables.
+
+    Returns
+    -------
+    Nothing
+    """
+    # These are required to be present
+    args.dynamodb_region = os.getenv('AWS_REGION')
+    args.access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    args.secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    args.loader_token = os.getenv('SVC_LOADER_TOKEN')
+    args.dynamodb_url = os.getenv('DYNAMODB_URL')
+
+
+def setup(args):
+    """Create the DynamoDB tables.
+
+    Parameters
+    ----------
+    args: namespace
+        The arguments specifying the tables. Uses dynamodb_url,
+        dynamodb_region, access_key_id, secret_access_key, table_suffix.
+    """
+    create_tables.create_tables(
+        args.dynamodb_url,
+        args.dynamodb_region,
+        args.access_key_id,
+        args.secret_access_key,
+        'Music-' + args.table_suffix,
+        'User-' + args.table_suffix,
+        'PlayList-' + args.table_suffix
+    )
+
+
+def run_test(args):
+    """Run the tests.
+
+    Parameters
+    ----------
+    args: namespace
+        The arguments for the test. Uses music_url.
+
+    Prerequisites
+    -------------
+    The DyamoDB tables must already exist.
+
+    Returns
+    -------
+    number
+        An HTTP status code representing the test result.
+        Some "pseudo-HTTP" codes are defined in the 600 range
+        to indicate conditions that are not included in the HTTP
+        specification.
+
+    Notes
+    -----
+    This test is highly incomplete and needs substantial extension.
+    """
+    mserv = music.Music(args.music_url, DUMMY_AUTH)
+    artist, song = ('Mary Chapin Carpenter', 'John Doe No. 24')
+    trc, m_id = mserv.create_song(artist, song)
+    assert trc == 200
+    trc, ra, rs = mserv.read_song(m_id)
+    assert (trc == 200 and artist == ra and song == rs)
+    trc = mserv.delete_song(m_id)
+    assert trc == 200
+
+    userv = user.User(args.user_url, DUMMY_AUTH)
+    fname, lname, email = ('Mohamadreza', 'Dokhah', 'mda85@sfu.ca')
+    trc, u_id = userv.create_user(fname, lname, email)
+    assert trc == 200
+    trc, rf, rl, re = userv.get_user(u_id)
+    assert (trc == 200 and fname == rf and lname == rl, email == re)
+    fname, lname, email = ('Mohammadreza', 'Dorkhah', 'mda84@sfu.ca')
+    trc, rf, rl, re = userv.update_user(u_id, fname, lname, email)
+    assert (trc == 200 and fname == rf and lname == rl, email == re)
+    trc, rf, rl, re = userv.get_user(u_id)
+    assert (trc == 200 and fname == rf and lname == rl, email == re)
+    trc = userv.delete_user(u_id)
+    assert trc == 200
+    trc = userv.login(u_id)
+    assert trc == 200
+    trc = userv.logoff()
+    assert trc == 200
+
+    pserv = playlist.PlayList(args.playlist_url, DUMMY_AUTH)
+    title, songs = ('My Favourites', [])
+    trc, p_id = pserv.create_playlist(title, songs)
+    assert trc == 200
+    trc, rt, rs = pserv.get_playlist(p_id)
+    assert (trc == 200 and title == rt and songs == rs)
+    trc = pserv.add_song_to_playlist(p_id, m_id)
+    assert trc == 200
+    trc = pserv.remove_song_from_playlist(p_id, m_id)
+    assert trc == 200
+    trc = pserv.delete_playlist(p_id)
+    assert trc == 200
+
+    return trc
+
+if __name__ == '__main__':
+    args = parse_args()
+    get_env_vars(args)
+    setup(args)
+    trc = run_test(args)
+    if trc != 200:
+        sys.exit(1)
---
+++ ci/v2/playlist.py
@@ -0,0 +1,201 @@
+"""
+SFU CMPT 756
+Sample application---music service.
+"""
+
+# Standard library modules
+import logging
+import os
+import sys
+
+# Installed packages
+from flask import Blueprint
+from flask import Flask
+from flask import request
+from flask import Response
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import requests
+
+import simplejson as json
+
+# Local modules
+import unique_code
+
+# The unique exercise code
+# The EXER environment variable has a value specific to this exercise
+ucode = unique_code.exercise_hash(os.getenv('EXER'))
+
+# The application
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Playlist process')
+
+db = {
+    "name": "http://cmpt756db:30002/api/v1/datastore",
+    "endpoint": [
+        "read",
+        "write",
+        "delete",
+        "update"
+    ]
+}
+bp = Blueprint('app', __name__)
+
+
+@bp.route('/health')
+@metrics.do_not_track()
+def health():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/readiness')
+@metrics.do_not_track()
+def readiness():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/', methods=['GET'])
+def list_all():
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    # list all playlists here
+    return {}
+
+
+@bp.route('/<music_id>', methods=['GET'])
+def get_song(music_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    payload = {"objtype": "music", "objkey": music_id}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/', methods=['POST'])
+def create_playlist():
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        PlaylistTitle = content['title']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][1]
+    response = requests.post(
+        url,
+        json={"objtype": "playlist", "title": PlaylistTitle},
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/<playlist_id>', methods=['PUT'])
+def remove_song_from_playlist(playlist_id, music_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}), status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        PlaylistTitle = content['title']
+        Songs = content['songs']
+        Songs.remove(music_id)
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.put(
+        url,
+        params={"objtype": "playlist", "objkey": playlist_id},
+        json={"title": PlaylistTitle, "songs": Songs})
+    return (response.json())
+
+
+@bp.route('/<playlist_id>', methods=['GET'])
+def get_playlist(playlist_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    payload = {"objtype": "playlist", "objkey": playlist_id}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/<playlist_id>', methods=['DELETE'])
+def delete_playlist(playlist_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    url = db['name'] + '/' + db['endpoint'][2]
+    response = requests.delete(
+        url,
+        params={"objtype": "playlist", "objkey": playlist_id},
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+@bp.route('/<playlist_id>', methods=['PUT'])
+def add_song_to_playlist(playlist_id, music_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}), status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        title = content['title']
+        songs = content['songs']
+        songs = songs.append(music_id)
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.put(
+        url,
+        params={"objtype": "user", "objkey": playlist_id},
+        json={"title": title, "songs": songs})
+    return (response.json())
+
+
+
+# All database calls will have this prefix.  Prometheus metric
+# calls will not---they will have route '/metrics'.  This is
+# the conventional organization.
+app.register_blueprint(bp, url_prefix='/api/v1/playlist/')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        logging.error("missing port arg 1")
+        sys.exit(-1)
+
+    app.logger.error("Unique code: {}".format(ucode))
+    p = int(sys.argv[1])
+    # Do not set debug=True---that will disable the Prometheus metrics
+    app.run(host='0.0.0.0', port=p, threaded=True)
